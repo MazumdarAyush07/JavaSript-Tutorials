70e9ed0bc6c7214394549678f71076b85be7a23e
MazumdarAyush07 <mazumdarayush.2002@gmail.com>
2023-09-08 12:43:12 +0530

    create web manipulation

---
+++ WebMani.js
@@ -0,0 +1,11 @@
+console.log("This is a Website manipulation tutorial");
+let a = window;
+console.log(a);
+
+//alert("Hello Ayush");
+
+//a = prompt("Type your name");
+//console.log(a);
+
+//a = confirm("Are you sure you want to remain on this page?");
+//console.log(a);
